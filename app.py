03c6e96e323c3d01a33ef6edd0f136c31d72db41
Varshath11 <varshath11@gmail.com>
2026-04-27 19:18:17 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 
-app = Flask(__name__,template_folder="templates")
+app = Flask(__name__)
 
 # Function to get exchange rate
 def convert_currency(from_currency, to_currency, amount):
